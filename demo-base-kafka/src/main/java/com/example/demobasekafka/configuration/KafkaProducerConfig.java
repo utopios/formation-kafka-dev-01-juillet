83e257c012bc9b5260c2cf6a9e0f5e403577d89a
ihab abadi <ihab@utopios.net>
2024-07-01 13:57:06 +0200

    avec correction de la partie configuration

--- demo-base-kafka/src/main/java/com/example/demobasekafka/configuration/KafkaProducerConfig.java
+++ demo-base-kafka/src/main/java/com/example/demobasekafka/configuration/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -34,6 +35,7 @@ public class KafkaProducerConfig {
     }
 
     @Bean
+    @Primary
     public KafkaTemplate<String, String> kafkaTemplateAsync() {
         Map<String, Object> configProps = commonConfig();
         configProps.put(ProducerConfig.ACKS_CONFIG, "1");
